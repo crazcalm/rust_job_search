2c3f7a7a3476c93dba1659895ea33c9843d070ed
Marcus Willock <crazcalm@gmail.com>
2025-11-08 18:34:15 -0500

    Need to take a break
    Next steps:
    - Figure out how I want to go about updating thand saving the models.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # rust_job_search
 Rust Port of an old project of mine
+
+## Migrations
+[refinery-cli](https://github.com/rust-db/refinery/tree/main/refinery_cli) is the migrations runner.
+
+### Install refinery-cli
+Install with `cargo install refinery_cli`
+
+### Create empty sqlite3 database
+This step is important because refinery expects the database to already exist. The location of the database is mentioned in the refinery.toml config file. For now, the expected location is `/home/crazcalm/.config/rust-job-search/rust_job_search.sqlite3"`. 
+
+Note: I have had trouble using `~` in the database 
+
+`sqlite3 rust_job_search.sqlite3 "Vacuum;"`
+
+Run migrations with `refinery `
--- src/models/company.rs
+++ src/models/company.rs
@@ -11,7 +11,45 @@ pub struct Company {
 
 #[cfg(test)]
 mod test {
+    use refinery::{embed_migrations, Migration};
+    use rusqlite::Connection;
+
     use super::*;
+
+    /*
+    use log::info;
+    use refinery::Migration;
+    use rusqlite::Connection;
+
+    refinery::embed_migrations!("migrations");
+
+    fn main() {
+        let mut conn = Connection::open_in_memory().unwrap();
+
+        // or run all migrations in one go
+        migrations::runner().run(&mut conn).unwrap();
+
+    }
+        */
+
+    fn run_migrations() {
+        refinery::embed_migrations!("migrations");
+        let mut conn = Connection::open_in_memory().unwrap();
+        migrations::runner().run(&mut conn).unwrap();
+
+        /*
+                You can verify that the migrations are running with
+                `cargo test -- --no-capture`
+
+            The first table stuff get printed to stdout.
+
+        TODO: figure out how I want to go about saving stuff...
+             */
+        let _ = conn.query_row("select * from sqlite_master;", [], |row| {
+            println!("{row:?}");
+            Ok(())
+        });
+    }
 
     #[test]
     fn test_create_default_instance() {
@@ -25,5 +63,10 @@ mod test {
         let result: Company = Default::default();
 
         assert!(expected == result);
+    }
+
+    #[test]
+    fn test_run_migrations() {
+        run_migrations();
     }
 }
